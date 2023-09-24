b2e283b6c229e48d7a507915be84564b328af65d
Dipta Pal <diptapal1998@gmail.com>
2023-09-24 14:01:37 +0600

    done

---
+++ src/components/Inventory/Inventory.jsx
@@ -0,0 +1,9 @@
+const Inventory = () => {
+    return (
+        <div>
+            <h2 className='text-center'>This is Inventory</h2>
+        </div>
+    );
+};
+
+export default Inventory;
---
+++ src/components/Shipping/Shipping.jsx
@@ -0,0 +1,11 @@
+const Shipping = () => {
+    return (
+        <div>
+            <h2>Shipping</h2>
+            <p>Delivery on the way</p>
+            <p><small>Coming Soon....</small></p>
+        </div>
+    );
+};
+
+export default Shipping;
